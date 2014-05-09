3a21f6d640ab0ed501e225f53691b19478068828
so92 <sowensdungiven@gmail.com>
2014-05-09 12:00:25 +0100

    refactoring

--- js/help.js
+++ js/help.js
@@ -1,19 +1,19 @@
 function showHelp(help) {
     if (help == 0) {
-        alert("Home");
+        alert("Choose either New User of Existing User");
     } else if (help == 1) {
-        alert("New User");
+        alert("Please Enter your Forname and Surname and click 'Enter with New User' to proceed");
     } else if (help == 2) {
-        alert("Existing User");
+        alert("You can search for your name using the search box, and click Select User to proceed");
     } else if (help == 3) {
-        alert("Main Menu");
+        alert("Choose an Option from one off the below butttons");
     } else if (help == 4) {
-        alert("During Quiz");
+        alert("Please select your answer by clicking on the box beside answer, click 'Next Question' to proceed");
     } else if (help == 5) {
-        alert("Last Questin");
+        alert("Please select your answer by clicking on the box beside answer, click 'Finish Quiz' to proceed");
     } else if (help == 6) {
-        alert("Answers Displayed");
+        alert("If you wish you may view leaderboard or return to Quiz Main menu, using the above buttons");
     } else if (help == 7) {
-        alert("Leaderboard");
+        alert("You may search the leaderboard, or return to Main Menu using button");
     }
 }
